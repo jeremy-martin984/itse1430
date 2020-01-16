c209619d72617066bef7e0bb96090be4326a7361
Jeremy Martin <jeremy.martin887@my.tccd.edu>
2020-01-15 21:39:28 -0600

    Day 2

--- classwork/Section1/Program.cs
+++ classwork/Section1/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿/*
+ * ITSE 1430
+ * Jeremy Martin
+ */
+
+using System;
 
 namespace Section1
 {
     class Program
     {
+
         static void Main ( string[] args )
         {
+
+
+            int test = 0;
             Console.WriteLine("Hello World!");
+            
+
         }
     }
 }
