59c7ec393945fe2e6c34922aa94d39bac950a27c
Jeremy Martin <jeremy.martin887@my.tccd.edu>
2020-01-29 19:53:40 -0600

    Story 1

--- classwork/Section1/Program.cs
+++ classwork/Section1/Program.cs
@@ -11,7 +11,7 @@ namespace Section1
         static void Main ( string[] args )
         {
             //
-            //PlayingWithVariables();                       
+            //PlayingWithVariables();                          
             AddMovie();
         }
         static void AddMovie ()
---
+++ labs/Lab 1/PizzaCreator/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Jeremy Martin
+ * ITSE 1430
+ * LAB 1
+ * Pizza Creator
+ */
+
+using System;
+
+namespace PizzaCreator
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            Console.WriteLine("Lab 1: Pizza Creator");
+        }
+    }
+}
