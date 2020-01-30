426bf6354705d90304d4db734011cc037661fdee
Jeremy Martin <jeremy.martin887@my.tccd.edu>
2020-01-29 21:30:47 -0600

    class 1/29

--- classwork/Section1/Program.cs
+++ classwork/Section1/Program.cs
@@ -10,94 +10,208 @@ namespace Section1
     {
         static void Main ( string[] args )
         {
-            //
-            //PlayingWithVariables();                          
-            AddMovie();
+            if (args==null)
+                throw new ArgumentNullException(nameof(args));
+            //PlayingWithVariables();  
+            var done = false;
+            do
+            {
+                switch (DisplayMenu())
+                {
+                    case Command.Add: AddMovie(); break;
+                    case Command.Display: DisplayMovie(); break;
+                    case Command.Quit: done = true; break;
+                };
+            } while (!done);
         }
+
+        private static void DisplayMovie ()
+        {
+            if (String.IsNullOrEmpty(title))
+            {
+                Console.WriteLine("No movies");
+                return;
+            };
+
+            Console.WriteLine(title);
+
+            Console.WriteLine(releaseYear);
+
+            Console.WriteLine(runLength + " (min)");
+
+            Console.WriteLine(isClassic ? "Classic" : "Not Classic");
+
+            if (!String.IsNullOrEmpty(description))
+                Console.WriteLine(description);
+        }
+        static void PlayingWithStrings()
+        {
+            var firstName = "Bob";
+            var lastName = "Jones";
+
+            var fullName = firstName + lastName;
+            fullName += " Jr";
+            //more than 6 string concatonations
+            var concat = String.Concat(firstName, lastName);
+            var joined = String.Join(" ", firstName, lastName);
+
+            Console.WriteLine("First Name: {0} Last Name: {1}", firstName, lastName);
+            var format = String.Format("First Name: {0} Last Name: {1}", firstName, lastName);
+
+            var format2 = "First Name: " + firstName + " Last Name: " + lastName;
+
+            //Preferred - string interpolation
+            var format3 = $"First Name: {firstName} Last Name: {lastName}";
+
+            var path = @"c:\windodws\system32"; // verbatim
+
+            var startsWithSlash = path.StartsWith(@"\");
+            var endswithSlash = path.EndsWith(@"\");
+
+            //padding
+
+            var padLeft = fullName.PadLeft(20);
+            var padRight = fullName.PadRight(20);
+            var header = "".PadLeft(20, '-'); // -------------------------------------------
+
+            var trimLeft = fullName.Trim(); //TrimStart();, TrimEnd();Trim('\special char here\')
+            var tokens = path.Split('\\');
+
+
+
+
+        }
+        enum Command
+        {
+            Quit = 0,
+            Add = 1,
+            Display = 2,
+        }
+
+        private static Command DisplayMenu ()
+        {
+            do
+            {
+                Console.WriteLine("A)dd Movie");
+                Console.WriteLine("D)isplay Movie");
+                Console.WriteLine("Q)uit");
+
+                var input = Console.ReadLine();
+                switch (input.ToLower())
+                {
+                    case "a": return Command.Add;
+                    case "d": return Command.Display;
+                    case "q": return Command.Quit;
+
+                    default: Console.WriteLine("Invalid option"); break;
+                };
+            } while (true);
+        }
+
+        static string title;
+        static int releaseYear;
+        static int runLength;
+        static string description;
+        static bool isClassic;
+
         static void AddMovie ()
         {
-            string title = ReadString("Enter a title: ", true);
+            title = ReadString("Enter a title: ", true);
 
-            int releaseYear = ReadInt32("Enter the release year (>= 0): ", 0, 2100);
-            int runLength = ReadInt32("Enter the run length (>= 0): ", 0, 86400);
+            releaseYear = ReadInt32("Enter the release year (>= 0): ", 0, 2100);
+            runLength = ReadInt32("Enter the run length (>= 0): ", 0, 86400);
 
-            string description = ReadString("Enter a description: ", false);
-            bool isClassic = ReadBoolean("Is this a classic movie?");
+            description = ReadString("Enter a description: ", false);
+            isClassic = ReadBoolean("Is this a classic movie?");
         }
 
         private static bool ReadBoolean ( string message )
         {
             Console.Write(message + " (Y/N)");
+
             do
             {
-                //Check for empty string
-                //if value != String.Empty another option
-                // if(value.Length == 0)
-                // if(!String.IsNullOrEmpty(value)) preferred
-               string value = Console.ReadLine();
-               // string value = Console.ReadKey(true);
+                string value = Console.ReadLine();
 
-                //if (value !="")
+                //Checking for empty string
+                // 1. if (value != "")
+                // 2. if (value != String.Empty)
+                // 3. if (value != null && value.Length == 0)
+                // 4. Preferred - String.IsNullOrEmpty
                 if (!String.IsNullOrEmpty(value))
-                    {
-                    //TODO: Do this correctly?
-                    if (String.Compare(value, "Y", true) == 0)
-                        return true;
-                    else if (String.Compare(value, "N", true) == 0)
-                        return false;
-                    
-
-                    char firstChar = value[0];
-
+                {
+                    // Input validation
+                    // 1. If
+                    // 2. Switch
+                    // 3. String casing
+                    // 4. String comparison
                     //value = value.ToLower();
                     //if (value == "y")
                     //    return true;
                     //else if (value == "n")
                     //    return false;
-                    //switch (firstChar)
-                    //{
-                    //  //  case 'A': Console.WriteLine("A"); break;
-                    //  //  case 'a': Console.WriteLine("a"); break;
-                    //    case 'Y': return true;
-                    //    case 'y': return true;
-                    //    case 'n': return false;
-                    //    case 'N': return false;
 
-                    //};
+                    //bool isYes = String.Compare(value, "Y", true) == 0 ? true : false;
+
+                    if (String.Compare(value, "Y", true) == 0)
+                        return true;
+                    else if (String.Compare(value, "N", true) == 0)
+                        return false;
+
+                    char firstChar = value[0];
                     //if (firstChar == 'Y' || firstChar == 'y')
                     //    return true;
                     //else if (firstChar == 'N' || firstChar == 'n')
-                    //    return false;
+                    //    return false;                    
+                    //switch (firstChar)
+                    //{
+                    //    #region Demo case
+                    //    //case 'A':
+                    //    //{
+                    //    //    Console.WriteLine("A");
+                    //    //    break;
+                    //    //};
+                    //    //case 'a': Console.WriteLine("a"); break;
+                    //    #endregion
+
+                    //    case 'Y':
+                    //    case 'y': return true;
+
+                    //    case 'N': 
+                    //    case 'n': return false;
+                    //};
                 };
+
                 Console.WriteLine("Enter Y/N");
             } while (true);
-
         }
 
         private static string ReadString ( string message, bool required )
         {
             Console.Write(message);
+
             do
             {
+                //var x = 10;
                 string value = Console.ReadLine();
 
+                //If required and string is empty then error
                 if (!String.IsNullOrEmpty(value) || !required)
                     return value;
 
                 if (required)
                     Console.WriteLine("Value is required");
-
-                return value;
             } while (true);
         }
 
         private static int ReadInt32 ( string message, int minValue, int maxValue )
         {
+            Console.Write(message);
+
             do
             {
-                Console.Write(message);
-
-                string temp = Console.ReadLine();
+                //string temp = Console.ReadLine();
+                var temp = Console.ReadLine();
                 //int value = Int32.Parse(temp);
 
                 //TODO: Clean this up
@@ -110,7 +224,41 @@ namespace Section1
 
                 Console.WriteLine("Value must be between minValue and maxValue");
             } while (true);
-
         }
+
+        //private static void PlayingWithVariables ()
+        //{
+        //    Console.WriteLine("Hello World!");
+
+        //    int hours;
+        //    double payRate;
+
+        //    {
+        //        string name;
+        //        bool pass;
+        //    }
+
+        //    {
+        //        int name = 10;
+        //    }
+
+        //    //Not cased properly
+        //    //string FirstName;
+
+        //    //hours = 10;
+        //    //int newHours = hours;
+
+        //    //Logical block 1     
+        //    int hours2 = 10;
+        //    //int hours3;
+        //    //hours3 = 3;
+
+        //    int x, y, z;
+        //    int a = 10, b = 20, c = 30;
+
+        //    //Display a message
+        //    Console.WriteLine("Enter a value");
+        //    string input = Console.ReadLine();
+        //}
     }
 }
