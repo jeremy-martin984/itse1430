db35df2d6190237d3b8167dbe3ede00a3d45fed4
Jeremy Martin <jeremy.martin887@my.tccd.edu>
2020-02-03 21:43:07 -0600

    namespaces

---
+++ classwork/MovieLibrary.Business/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibrary.Business
+{
+    class Movie
+    {
+        //public/internal class id
+        string title;
+
+        int runLength;
+
+        string decription;
+
+        int releaseYear;
+
+        bool isClassic;
+        //title
+        //run length
+        //description
+        //release year
+        //is classic
+    }
+}
