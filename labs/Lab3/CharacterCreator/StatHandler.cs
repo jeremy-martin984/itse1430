9350a249f11fb73c3af25c3298aa27798b1cb60f
jeremy-martin984 <55176671+jeremy-martin984@users.noreply.github.com>
2020-03-07 21:05:47 -0600

    3/7

---
+++ labs/Lab3/CharacterCreator/StatHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterCreator
+{
+    /// <summary>
+    /// Takes two strings from text field, parses as ints, and returns new values (as strings) when appropriate.
+    /// </summary>
+    public class StatHandler
+    {
+        public void PlusStat ( string pointsRemaining, string statIncrease, out string newPointsRemaining, out string newStatTotal )
+        {
+            var temp = Int32.Parse(pointsRemaining);
+            if (temp == 0)
+            {
+                newPointsRemaining = null;
+                newStatTotal = null;
+                return;
+            } else
+            {
+                temp -= 1;
+                newPointsRemaining = temp.ToString();
+                temp = Int32.Parse(statIncrease);
+                temp += 1;
+                newStatTotal = temp.ToString();
+                return;
+            }
+        }
+
+        public void MinusStat ( string pointsRemaining, string statIncrease, out string newPointsRemaining, out string newStatTotal )
+        {
+            var temp = Int32.Parse(statIncrease);
+            if (temp == 50)
+            {
+                newPointsRemaining = null;
+                newStatTotal = null;
+                return;
+            } else
+            {
+                temp -= 1;
+                newStatTotal = temp.ToString();
+                temp = Int32.Parse(pointsRemaining);
+                temp += 1;
+                newPointsRemaining = temp.ToString();
+                return;
+            }
+        }
+    }
+}
